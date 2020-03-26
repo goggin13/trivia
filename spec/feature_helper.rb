a6b8bc86d2f7759b243f3578d7edcd185a3ff7ae
goggin13 <goggin13@gmail.com>
2020-03-26 18:29:45 -0500

    no passwords needed

--- spec/feature_helper.rb
+++ spec/feature_helper.rb
@@ -1,8 +1,8 @@
 def sign_in(user)
   visit "/users/sign_in"
   fill_in 'Email', with: user.email
-  fill_in 'Password', with: user.password
-  click_button 'Log in'
+  # fill_in 'Password', with: user.password
+  click_button 'Sign in'
   expect(page).to have_content("Signed in successfully.")
 end
 
