002d059686a8b7ec5e4626dce3a03fbb8b5821d1
goggin13 <goggin13@gmail.com>
2020-03-29 11:58:58 -0500

    initial questions

--- slurp.rb
+++ slurp.rb
@@ -1,4 +1,14 @@
+
+round = nil
+if Round.count == 0
+  round = Round.create!(:label => "Round #{Round.count + 1}")
+end
+
 JSON.parse(File.read("json_questions.json").chomp).each do |question|
   puts question
-  puts Question.create_from_json(question)
+  if round.questions.count > Round.questions_per_round
+    round = Round.create!(:label => "Round #{Round.count + 1}")
+    puts "Creating new round #{round.label}"
+  end
+  puts Question.create_from_json(question, round)
 end
